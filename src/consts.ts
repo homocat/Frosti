3e009d3b34bea05361e7d49c33eb04704424cc31
fool-homocat <luodan_0464@qq.com>
2024-07-21 21:18:41 +0800

    feat: add anime; post: kininonamaewa

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'homocat.fun';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const USER_NICKNAME = 'EveSunMaple';
-export const USER_DESCRIPTION = 'a student';
+export const USER_NICKNAME = 'homocat';
+export const USER_DESCRIPTION = '丛雨天下第一';
 export const TRANSITION_API = true;
